d0d51a410a7c4806593e1dce5de194fb928b5193
Olivier <oaudetyang@gmail.com>
2022-03-15 18:07:45 -0400

    Removed PWA

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ const withPWA = require('next-pwa');
 module.exports = withPWA({
   reactStrictMode: true,
   productionBrowserSourceMaps: true,
-  pwa: {
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-  },
   env: {
     GOOGLE_CLIENT_ID:
       '645889149447-0ouqtjhrg9mpnp2amjrb05ievs255dn1.apps.googleusercontent.com',
