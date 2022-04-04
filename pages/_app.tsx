6928f0b9ae6fa0e0698b4eb4e67baf31bd476d86
Olivier Audet-Yang <66892647+greentext01@users.noreply.github.com>
2022-04-03 20:57:45 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { CssBaseline } from '@mui/material';
 import { AppProps } from 'next/dist/shared/lib/router/router';
 import Head from 'next/head';
+import { useEffect } from React;
 
 function MyApp({ Component, pageProps }: AppProps) {
   const theme = createTheme({
@@ -10,7 +11,9 @@ function MyApp({ Component, pageProps }: AppProps) {
     },
   });
   
-  window.location.href = "https://gameslist-with-shrimp.herokuapp.com/";
+  useEffect({
+    window.location.href = "https://gameslist-with-shrimp.herokuapp.com/";
+  }, []);
 
   return (
     <>
